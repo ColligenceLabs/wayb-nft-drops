0e6dd08ce83380b9f2f31085b074e5edc27a3445
Vinhphan1609 <viinhphan132@gmail.com>
2022-09-13 09:29:46 +0700

    Update LandingLayout.jsx

--- src/components/layouts/studioLayout/LandingLayout.jsx
+++ src/components/layouts/studioLayout/LandingLayout.jsx
@@ -1,5 +1,4 @@
-import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
+import Footer from 'components/footer/Footer';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
