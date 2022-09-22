19ad9415ee26cc1304a1a03ffb8c43212d0b2f5c
Hung Nguyen <67354423+hungpy01199@users.noreply.github.com>
2022-09-22 13:27:55 +0700

    update button back

--- src/pages/MyCollectibles/MyCollectiblesDetailsMB.jsx
+++ src/pages/MyCollectibles/MyCollectiblesDetailsMB.jsx
@@ -18,7 +18,7 @@ import rare_lg from '../../assets/svg/rare_logo.svg';
 import ic_authenticity from '../../assets/svg/authenticity_icon.svg';
 import price_history_lg from '../../assets/svg/price_history_logo.svg';
 import ic_trade from '../../assets/svg/trade_icon.svg';
-import ic_sell from '../../assets/svg/sell_icon.svg'
+import ic_sell from '../../assets/svg/sell_icon.svg';
 
 import { useState, useEffect, useRef } from 'react';
 import Popup from 'reactjs-popup';
@@ -27,154 +27,176 @@ import 'slick-carousel/slick/slick-theme.css';
 import 'react-multi-carousel/lib/styles.css';
 
 const MyCollectiblesDetailsMB = () => {
-    const [open, setOpen] = useState(false);
-    const [popup, setPopUp] = useState(false);
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const [scrollPositionPercent, setScrollPositionPercent ] = useState(0)
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollPosition(position);
-    };
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-    
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-    const handeScrollPercent = () => {
-        const positionPercent = (window.pageYOffset / (document.documentElement.offsetHeight - window.innerHeight)) * 100;
-        setScrollPositionPercent(positionPercent);
-    }
-    useEffect(() => {
-        window.addEventListener("scroll", handeScrollPercent);
-    
-        return () => {
-          window.removeEventListener("scroll", handeScrollPercent);
-        };
-    }, []);
-    console.log(scrollPositionPercent);
-    // console.log(scrollPosition);
-    return (
-        <main className="collectibles-details-container-mb">
-            <div className="collectibles-details-wp">
-                <div className="product-details">
-                    <div className="showcase-box">
-                    <Link to={'/my-collectibles'}>
-                        <button className="back-button">
-                            <img src={ic_back} alt="back-icon" /> Back
-                        </button>
-                    </Link>
-                    <img src="https://collectible.sweet.io/series/1727/image-front.png" alt=""/>
-            {/* <canvas className="canvas-card" width="1125" height="1125" style={{ width: '900px', height: '900px' }}></canvas> */}
-                    </div>
+  const [open, setOpen] = useState(false);
+  const [popup, setPopUp] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPositionPercent, setScrollPositionPercent] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
 
-                    <div className="details-box">
-                        <div className="banner-dropdown">
-                            <div className="logo">
-                                <img src="https://static.assets.sweet.io/campaigns/267/avatar.jpg" alt="Sweet" className="logo-img"/>
-                                <div className="logo-info">
-                                    <div className="creator">Creator</div>
-                                    <div className="name">Sweet</div>
-                                </div>
-                            </div>
-                            <div className="dropdown">
-                                <div className="dropdown-button" onClick={() => setOpen(open => !open)}>
-                                    <img src={ic_dropdown} alt="dropdown" />
-                                </div>
-                                {open && (
-                                <ul className="dropdown-box">
-                                    <li className="list-dropdown-item">
-                                        <button className="dropdown-item ">
-                                            <img src={ic_send_to_my_wallet} alt="send-to-my-wallet" />
-                                            Send to My Wallet
-                                        </button>
-                                    </li>
-                                    <li className="list-dropdown-item">
-                                        <button className="dropdown-item">
-                                            <img src={ic_show_off} alt="show-off" />
-                                            Show off
-                                        </button>
-                                    </li>
-                                    
-                                </ul>
-                                )}
-                            </div>
-                        </div>
-                        <div className="label-name">
-                            Strawberry Shortcake Space Creampop
-                            <div className="rarity-label">
-                                <img src={rare_lg} alt="rare-logo" />
-                                RARE
-                            </div>
-                        </div>
-                        <div className="description-label"> The only thing better than ice cream is ice cream in SPACE!</div>
-                        <a target="_blank" href="https://polygonscan.com/token/0xF3e34e2022029A7eCb38d7373f7171f478670B20?a=48" className="authenticity-button">
-                            <img src={ic_authenticity} alt="authenticity-icon" />
-                            Authenticity
-                        </a>
-                        <div className="list-item">
-                            <div className="item">
-                                <div className="label">Number</div>
-                                <div className="value">49</div>
-                            </div>
-                            <div className="item">
-                                <div className="label" >Release Date</div>
-                                <div className="value" >8/29/2022</div>
-                            </div>
-                            <div className="item">
-                                <div className="label">Date Acquired</div>
-                                <div className="value">9/2/2022</div>
-                            </div>
-                            <div className="item">
-                                <div className="label">Total Run</div>
-                                <div className="value">2000</div>
-                            </div>
-                            <div className="item">
-                                <div className="label" data-qa-component="token-type-label">Token Type</div>
-                                <div className="value" data-qa-component="token-type-value">erc721</div>
-                            </div>
-                            <div className="item">
-                                <div className="label" >Network</div>
-                                <div className="value">Polygon</div>
-                            </div>
-                        </div>
-                        <div className="list-trade">
-                            <div style={{ position: 'relative' }}>
-                                    
-                                <button className="btn-trade status disabled">
-                                    
-                                        <img src={ic_sell} alt="sell" />
-                                        Sell on Sweet
-                                        
-                                </button>
-                                
-                            <div className="noti-cannot">This collectible cannot be currently sold on Sweet.</div> 
-                            
-                            
-                            </div>
-                
-                          
-                            <div>
-                            <button className="btn-trade status">
-                                <img src={ic_trade} alt="trade"/>
-                                Trade on Sweet
-                                
-                            </button>
-                            </div>
-                        </div>
-                        <div className="price-history">
-                            <div className="price-history-label">
-                                <img src={price_history_lg } alt="price-history" />
-                                Price History
-                            </div>
-                            <div className="list-price-history"></div>
-                        </div>
-                    </div>
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const handeScrollPercent = () => {
+    const positionPercent =
+      (window.pageYOffset /
+        (document.documentElement.offsetHeight - window.innerHeight)) *
+      100;
+    setScrollPositionPercent(positionPercent);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handeScrollPercent);
+
+    return () => {
+      window.removeEventListener('scroll', handeScrollPercent);
+    };
+  }, []);
+  console.log(scrollPositionPercent);
+  // console.log(scrollPosition);
+  return (
+    <main className="collectibles-details-container-mb">
+      <div className="collectibles-details-wp">
+        <div className="product-details">
+          <div className="showcase-box">
+            <Link to={'/my-collectibles'}>
+              <button className="back-button">
+                <img src={ic_back} alt="back-icon" /> Back
+              </button>
+            </Link>
+            <img
+              src="https://collectible.sweet.io/series/1727/image-front.png"
+              alt=""
+              className="thumbnail"
+            />
+            {/* <canvas className="canvas-card" width="1125" height="1125" style={{ width: '900px', height: '900px' }}></canvas> */}
+          </div>
+
+          <div className="details-box">
+            <div className="banner-dropdown">
+              <div className="logo">
+                <img
+                  src="https://static.assets.sweet.io/campaigns/267/avatar.jpg"
+                  alt="Sweet"
+                  className="logo-img"
+                />
+                <div className="logo-info">
+                  <div className="creator">Creator</div>
+                  <div className="name">Sweet</div>
                 </div>
+              </div>
+              <div className="dropdown">
+                <div
+                  className="dropdown-button"
+                  onClick={() => setOpen((open) => !open)}
+                >
+                  <img src={ic_dropdown} alt="dropdown" />
+                </div>
+                {open && (
+                  <ul className="dropdown-box">
+                    <li className="list-dropdown-item">
+                      <button className="dropdown-item ">
+                        <img
+                          src={ic_send_to_my_wallet}
+                          alt="send-to-my-wallet"
+                        />
+                        Send to My Wallet
+                      </button>
+                    </li>
+                    <li className="list-dropdown-item">
+                      <button className="dropdown-item">
+                        <img src={ic_show_off} alt="show-off" />
+                        Show off
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </div>
             </div>
-        </main>
-    );
-}
+            <div className="label-name">
+              Strawberry Shortcake Space Creampop
+              <div className="rarity-label">
+                <img src={rare_lg} alt="rare-logo" />
+                RARE
+              </div>
+            </div>
+            <div className="description-label">
+              {' '}
+              The only thing better than ice cream is ice cream in SPACE!
+            </div>
+            <a
+              target="_blank"
+              href="https://polygonscan.com/token/0xF3e34e2022029A7eCb38d7373f7171f478670B20?a=48"
+              className="authenticity-button"
+            >
+              <img src={ic_authenticity} alt="authenticity-icon" />
+              Authenticity
+            </a>
+            <div className="list-item">
+              <div className="item">
+                <div className="label">Number</div>
+                <div className="value">49</div>
+              </div>
+              <div className="item">
+                <div className="label">Release Date</div>
+                <div className="value">8/29/2022</div>
+              </div>
+              <div className="item">
+                <div className="label">Date Acquired</div>
+                <div className="value">9/2/2022</div>
+              </div>
+              <div className="item">
+                <div className="label">Total Run</div>
+                <div className="value">2000</div>
+              </div>
+              <div className="item">
+                <div className="label" data-qa-component="token-type-label">
+                  Token Type
+                </div>
+                <div className="value" data-qa-component="token-type-value">
+                  erc721
+                </div>
+              </div>
+              <div className="item">
+                <div className="label">Network</div>
+                <div className="value">Polygon</div>
+              </div>
+            </div>
+            <div className="list-trade">
+              <div style={{ position: 'relative' }}>
+                <button className="btn-trade status disabled">
+                  <img src={ic_sell} alt="sell" />
+                  Sell on Sweet
+                </button>
+
+                <div className="noti-cannot">
+                  This collectible cannot be currently sold on Sweet.
+                </div>
+              </div>
+
+              <div>
+                <button className="btn-trade status">
+                  <img src={ic_trade} alt="trade" />
+                  Trade on Sweet
+                </button>
+              </div>
+            </div>
+            <div className="price-history">
+              <div className="price-history-label">
+                <img src={price_history_lg} alt="price-history" />
+                Price History
+              </div>
+              <div className="list-price-history"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MyCollectiblesDetailsMB;
--- src/pages/MyCollectibles/MyCollectiblesPC.jsx
+++ src/pages/MyCollectibles/MyCollectiblesPC.jsx
@@ -20,222 +20,219 @@ import 'react-multi-carousel/lib/styles.css';
 import MyCollectiblesMB from './MyCollectiblesMB';
 
 const MyCollectibles = () => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [appState, setChange] = useState({
-        
-        objects: [{
-            id: 1,
-            name: 'Newest'
-        },
-        {
-            id: 2,
-            name: 'Oldest'
-        },
-        {
-            id: 3,
-            name: 'Title: A-Z'
-        },
-        {
-            id: 4,
-            name: 'Title: Z-A'
-        }],
-        activeObject: null,
-        activeElement : 0
-    });
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [appState, setChange] = useState({
+    objects: [
+      {
+        id: 1,
+        name: 'Newest',
+      },
+      {
+        id: 2,
+        name: 'Oldest',
+      },
+      {
+        id: 3,
+        name: 'Title: A-Z',
+      },
+      {
+        id: 4,
+        name: 'Title: Z-A',
+      },
+    ],
+    activeObject: null,
+    activeElement: 0,
+  });
 
-    const listItems = [
-        {
-            id: 1,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_1
-        },
-        {
-            id: 2,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_09_avt,
-            icon: ic_collectible_2
-        },
-        {
-            id: 3,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_08_avt,
-            icon: ic_collectible_2
-        },
-        {
-            id: 4,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_09_avt,
-            icon: ic_collectible_1
-        },
-        {
-            id: 5,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_3
-        },
-        {
-            id: 6,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_3
-        },
-        {
-            id: 7,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_2
-        },
-        {
-            id: 8,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_1
-        },
-        {
-            id: 9,
-            name: 'Strawberry Shortcake Space Creampop',
-            image: home_10_avt,
-            icon: ic_collectible_2
-        },
-    ]
+  const listItems = [
+    {
+      id: 1,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_1,
+    },
+    {
+      id: 2,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_09_avt,
+      icon: ic_collectible_2,
+    },
+    {
+      id: 3,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_08_avt,
+      icon: ic_collectible_2,
+    },
+    {
+      id: 4,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_09_avt,
+      icon: ic_collectible_1,
+    },
+    {
+      id: 5,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_3,
+    },
+    {
+      id: 6,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_3,
+    },
+    {
+      id: 7,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_2,
+    },
+    {
+      id: 8,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_1,
+    },
+    {
+      id: 9,
+      name: 'Strawberry Shortcake Space Creampop',
+      image: home_10_avt,
+      icon: ic_collectible_2,
+    },
+  ];
 
-    const [open, setOpen] = useState(false);
-    const [selection, setSelection] = useState([]);
-    
-    const ref = useRef();
-    useEffect(() => {
-        let handler = (event) => {
-            if(!ref.current.contains(event.target)) {
-                setOpen(false);
-            }
-        };
-        document.addEventListener('mousedown', handler);
-        return () => {
-            document.removeEventListener('mousedown', handler);
-        }
-    }); 
+  const [open, setOpen] = useState(false);
+  const [selection, setSelection] = useState([]);
 
+  const ref = useRef();
+  useEffect(() => {
+    let handler = (event) => {
+      if (!ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handler);
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  });
 
-    function toggleActive(index) {
-        setChange({ ...appState, activeObject: appState.objects[index] })
+  function toggleActive(index) {
+    setChange({ ...appState, activeObject: appState.objects[index] });
+  }
+
+  function handleSortClick(item, multipleSelect = false) {
+    if (!selection.some((current) => current.id === item.id)) {
+      if (!multipleSelect) {
+        setSelection([item]);
+      } else if (multipleSelect) {
+        setSelection([...selection, item]);
+      }
+    } else {
+      let selectionAfterRemoval = selection;
+      selectionAfterRemoval = selectionAfterRemoval.filter(
+        (current) => current.id !== item.id
+      );
+      setSelection([...selectionAfterRemoval]);
     }
+  }
 
-    function handleSortClick(item, multipleSelect = false) {
-        if(!selection.some(current => current.id === item.id)) {
-            if(!multipleSelect) {
-                setSelection([item]);
-            } else if(multipleSelect) {
-                setSelection([...selection, item]);
-            } 
-        } else {
-            let selectionAfterRemoval = selection;
-            selectionAfterRemoval = selectionAfterRemoval.filter(
-                current => current.id !== item.id
-            );
-            setSelection([...selectionAfterRemoval]);
-        }
-        
+  function isItemInSelection(item) {
+    if (selection.find((current) => current.id === item.id)) {
+      return true;
     }
+    return false;
+  }
 
-    function isItemInSelection(item) {
-        if(selection.find(current => current.id === item.id)) {
-            return true;
-        }
-        return false;
-    }
-
-    return ( isMobile ? (
-        <MyCollectiblesMB />
-    ) : (
+  return isMobile ? (
+    <MyCollectiblesMB />
+  ) : (
     <main className="my-wallet-container">
-        <div className="wallet-wp">
-
-            {/* banner  */}
-            <div className="banner">
-                
-                <div className="collectibles">
-                    <h1 className="banner-title">My Collectibles</h1>
-                </div>
-                
-            </div>
-
-            {/* list button sort  */}
-            <div className="wallet-sort">
-                <div className="count-label">9 items</div>
-                
-                <div className="sort-box" ref={ref}>
-                    
-                    <span className="sort-selected">
-                    {appState.objects.map((item, index) => {
-                        return <div className={`${selectedIndex === index ? 'name-sort-selected' : 'not-selected'}`} key={index}>{item.name}</div>   
-                    })}
-                         
-                    </span>
-                    <div className="sort-dropdown-button" onClick={() => setOpen(open => !open)}>
-                        <img src={ic_dropdown_button_arrow} alt="" />
-                        
-                    </div>
-                    {open && (
-                        <ul className="sort-dropdown-box" >
-
-                        {appState.objects.map((item, index) => {
-                            
-                            return <li key={index} className="name-sort-dropdown-box">
-                                <button type="button" className="sort-button" onClick={() => {handleSortClick(item); toggleActive(index);
-                            setSelectedIndex(index);}}>
-                                    <span style={{ fontWeight: 'bold' }}>{item.name }</span>
-                                    <span>{isItemInSelection(item) && ''}</span>
-                                </button>    
-                            </li>
-                        })}
-                        </ul>
-                    )}
-                    
-                        
-                </div>
-            </div>
-
-            {/* item  */}
-            <div className="page-grid">
-            {listItems.map((item, index) => {
-                        
-                        
-            return <a href='/my-collectibles/details' key={index}>
-                <div className="item-product" >
-                        
-                    
-                    <div className="item-product-detail">
-                        <div className="card">
-                            <img src={home_10_avt} alt="" />
-                        </div>
-                    </div>
-                    
-                    <div className="item-product-detail" style={{ padding: '0px' }}>
-                        <div className="box-info">
-                            <div className="box-product-name">
-                                <div className="product-type">Sweet</div>
-                                <div className="product-name">
-                                {item.name}
-                                </div>
-                            </div>
-                            <img src={item.icon} alt="" />
-                                            
-                        </div>
-                    </div>
-                    
-                    <div className="item-product-detail">
-                        
-                    </div>
-                </div>
-            </a>
-            })}
-            </div>
+      <div className="wallet-wp">
+        {/* banner  */}
+        <div className="banner">
+          <div className="collectibles">
+            <h1 className="banner-title">My Collectibles</h1>
+          </div>
         </div>
-    </main>
-    )
-    );
-};
 
+        {/* list button sort  */}
+        <div className="wallet-sort">
+          <div className="count-label">9 items</div>
+
+          <div className="sort-box" ref={ref}>
+            <span className="sort-selected">
+              {appState.objects.map((item, index) => {
+                return (
+                  <div className={`${selectedIndex === index ? 'name-sort-selected' : 'not-selected'}`} key={index}>
+                    {item.name}
+                  </div>
+                );
+              })}
+            </span>
+            <div className="sort-dropdown-button" onClick={() => setOpen((open) => !open)}
+            >
+              <img src={ic_dropdown_button_arrow} alt="" />
+            </div>
+            {open && (
+              <ul className="sort-dropdown-box">
+                {appState.objects.map((item, index) => {
+                  return (
+                    <li key={index} className="name-sort-dropdown-box">
+                      <button
+                        type="button"
+                        className="sort-button"
+                        onClick={() => {
+                          handleSortClick(item);
+                          toggleActive(index);
+                          setSelectedIndex(index);
+                        }}
+                      >
+                        <span style={{ fontWeight: 'bold' }}>{item.name}</span>
+                        <span>{isItemInSelection(item) && ''}</span>
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+        </div>
+
+        {/* item  */}
+        <div className="page-grid">
+          {listItems.map((item, index) => {
+            return (
+              <a href="/my-collectibles/details" key={index}>
+                <div className="item-product">
+                  <div className="item-product-detail">
+                    <div className="card">
+                      <img src={home_10_avt} alt="" />
+                    </div>
+                  </div>
+
+                  <div
+                    className="item-product-detail"
+                    style={{ padding: '0px' }}
+                  >
+                    <div className="box-info">
+                      <div className="box-product-name">
+                        <div className="product-type">Sweet</div>
+                        <div className="product-name">{item.name}</div>
+                      </div>
+                      <img src={item.icon} alt="" />
+                    </div>
+                  </div>
+
+                  <div className="item-product-detail"></div>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default MyCollectibles;
