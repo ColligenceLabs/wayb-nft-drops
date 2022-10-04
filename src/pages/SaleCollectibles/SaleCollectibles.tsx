08f28c0a569d8f4158736c2de20f872b77a41156
helloljho@gmail.com <helloljho@gmail.com>
2022-10-04 16:31:28 +0900

    add connect wallet button

--- src/pages/SaleCollectibles/SaleCollectibles.tsx
+++ src/pages/SaleCollectibles/SaleCollectibles.tsx
@@ -19,76 +19,37 @@ import contracts from '../../config/constants/contracts';
 import { targetNetwork } from '../../config';
 import { useWeb3React } from '@web3-react/core';
 import { setupNetwork } from '../../utils/wallet';
+import WalletConnector from '../../components/auth/WalletConnector/WalletConnector';
+import Popup from 'reactjs-popup';
 
 type MBoxTypesWithCompany = MBoxTypes & {
   companyLogo: string;
   companyName: string;
 };
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-  },
-];
+const overlayStyle = { background: 'rgba(0,0,0,0.8)' };
+const closeOnDocumentClick = false;
+const lockScroll = true;
 
 const SaleCollectibles = () => {
   const location = useLocation();
 
   const { account, library } = useWeb3React();
+
   const [mBoxInfo, setMBoxInfo] = useState<MBoxTypesWithCompany | null>(null);
   const [mBoxItemList, setMBoxItemList] = useState<MBoxItemTypes[]>([]);
   const [isModalOpen, setModalOpen] = useState(false);
   const [openPaymentWallets, setOpenPaymentWallets] = useState(false);
   const [openPaymentWalletsSuccess, setOpenPaymentWalletsSuccess] =
     useState(false);
-  const [puzzleData, setPuzzleData] = useState(() =>
-    Array.from(Array(99).keys())
-  );
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [signupOpen, setSignupOpen] = useState(false);
+  const closeLogin = () => {
+    setLoginOpen(false);
+  };
+  const closeSignup = () => {
+    setSignupOpen(false);
+  };
 
   const ref = useRef();
   const useOnClickOutside = (ref: any, handler: any) => {
@@ -123,23 +84,27 @@ const SaleCollectibles = () => {
     }
   }, []);
 
-  const handleBuyClick = async (info: MBoxTypesWithCompany) => {
-    console.log('=======>', info);
-    if (account === undefined || library?.connection === undefined) {
-      // TODO : 지갑 연경 창을 띄워 줄 것...
-      console.log('### 준호야 지갑연결 창을 띄워 줘... ');
+  const handleBuyClick = async () => {
+    console.log('=======>', mBoxInfo);
+    if (mBoxInfo) {
+      if (account === undefined || library?.connection === undefined) {
+        // TODO : 지갑 연경 창을 띄워 줄 것...
+        console.log('### 준호야 지갑연결 창을 띄워 줘... ');
+      }
+      const payment = parseEther(
+        ((mBoxInfo.price ?? 0) * 1).toString()
+      ).toString();
+      const result = await buyKey(
+        mBoxInfo.boxContractAddress,
+        1,
+        payment,
+        mBoxInfo.quote === 'klay'
+          ? contracts.klay[targetNetwork]
+          : contracts.wklay[targetNetwork],
+        account,
+        library
+      );
     }
-    const payment = parseEther(((info.price ?? 0) * 1).toString()).toString();
-    const result = await buyKey(
-      info.boxContractAddress,
-      1,
-      payment,
-      info.quote === 'klay'
-        ? contracts.klay[targetNetwork]
-        : contracts.wklay[targetNetwork],
-      account,
-      library
-    );
   };
 
   return (
@@ -217,13 +182,22 @@ const SaleCollectibles = () => {
                     <div className="lable-top">Purchase price</div>
                     <div className="lable-bottom fw-600">{`${mBoxInfo.price} ${mBoxInfo.quote}`}</div>
                   </div>
-                  <button
-                    className={'btn-sale-collection'}
-                    // onClick={() => setOpenPaymentWallets(true)}
-                    onClick={async () => await handleBuyClick(mBoxInfo)}
-                  >
-                    Buy Now
-                  </button>
+                  {account && library?.connection ? (
+                    <button
+                      className={'btn-sale-collection'}
+                      // onClick={() => setOpenPaymentWallets(true)}
+                      onClick={handleBuyClick}
+                    >
+                      Buy Now
+                    </button>
+                  ) : (
+                    <button
+                      className={'btn-sale-collection'}
+                      onClick={() => setLoginOpen(true)}
+                    >
+                      Connect Wallet
+                    </button>
+                  )}
                   {/* <button className="btn-sale-collection disable">Sold out</button> */}
                 </div>
               </div>
@@ -369,6 +343,19 @@ const SaleCollectibles = () => {
             show={openPaymentWalletsSuccess}
             onHide={() => setOpenPaymentWalletsSuccess(false)}
           />
+          <Popup
+            modal
+            open={loginOpen}
+            onOpen={closeSignup}
+            onClose={closeLogin}
+            {...{ overlayStyle, closeOnDocumentClick, lockScroll }}
+          >
+            {/* <LoginForm close={closeLogin} onConfirm={() => setSignupOpen(true)} />*/}
+            <WalletConnector
+              close={closeLogin}
+              onConfirm={() => setSignupOpen(true)}
+            />
+          </Popup>
         </div>
       ) : null}
     </main>
