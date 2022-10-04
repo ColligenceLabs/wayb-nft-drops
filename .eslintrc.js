c24b58392db74c1ea5067a22bb3f487254f346b7
Peter H. Namh <hwnahm@gmail.com>
2022-10-04 16:03:48 +0900

    fix: solve caver-js webpack5 conflict

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-ignore': 'allow-with-description',
+        minimumDescriptionLength: 10,
+      },
+    ],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
--- src/components/common/AppStore.js
+++ src/components/common/AppStore.js
@@ -1,13 +1,12 @@
-import create from 'zustand'
+import create from 'zustand';
 
 export const useModalWalletsStore = create((set) => ({
   isOpenWallet: false,
-  updateOpenWallet: (isOpen) => set({isOpen: isOpen}),
-}))
+  updateOpenWallet: (isOpen) => set({ isOpen: isOpen }),
+}));
 
 export const useSidebarStore = create((set) => ({
   isOpenSidebar: false,
-  openSidebar: () => set({isOpen: true}),
-  closeSidebar: () => set({isOpen: false}),
-
-}))
+  openSidebar: () => set({ isOpen: true }),
+  closeSidebar: () => set({ isOpen: false }),
+}));
