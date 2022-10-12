1cf34fa28fe3ca8aeed5cb6ddcc214f9e78e59ed
SonLH <sonlh@chainos.vn>
2022-10-13 01:33:51 +0700

    update sale collectibles

--- src/components/card/MBoxItemCard.tsx
+++ src/components/card/MBoxItemCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { MBoxItemTypes } from '../../types/MBoxItemTypes';
 
 type MBoxItemCardProps = {
@@ -13,45 +14,47 @@ const MBoxItemCard: React.FC<MBoxItemCardProps> = ({
 }) => {
   return (
     <div className="slide-item">
-      {/*<Link to={'/sale'} className="button">*/}
-      <div className="hot-ollectibles-wrapper">
-        <div className="header-left hot-ollectibles-item">
-          {/*<span className="total-run fw-600">Total Run: 35000</span>*/}
-        </div>
-        <div className="hot-ollectibles-item">{/*<div>erc721</div>*/}</div>
-        <div className="hot-ollectibles-item">
-          <div className="img-token">
-            <img src={item.itemImage} alt="" />
+      <Link to={'/sale'} className="button">
+        <div className="hot-ollectibles-wrapper">
+          <div className="header-left hot-ollectibles-item">
+            <span className="total-run fw-600">Total Run: 35000</span>
           </div>
-        </div>
-        <div className="hot-ollectibles-item">
-          <div className="wrapper-item">
-            <div className="content-left">
-              <div className="avatar">
-                <img src={mBoxImage} alt="" />
-              </div>
-              <div className="name-label">{mBoxName}</div>
+          <div className="hot-ollectibles-item">
+            <div>erc721</div>
+          </div>
+          <div className="hot-ollectibles-item">
+            <div className="img-token">
+              <img src={item.itemImage} alt="" />
             </div>
-            {/*<div className="content-right">Buy Now</div>*/}
+          </div>
+          <div className="hot-ollectibles-item">
+            <div className="wrapper-item">
+              <div className="content-left">
+                <div className="avatar">
+                  <img src={mBoxImage} alt="" />
+                </div>
+                <div className="name-label">{mBoxName}</div>
+              </div>
+              <div className="content-right">Buy Now</div>
+            </div>
+          </div>
+          <div className="hot-ollectibles-item">
+            <div className="name-label">{item.name}</div>
+          </div>
+          <div className="hot-ollectibles-item">
+            <div className="wrapper-price">
+              <div className="price-header font-size-14">Price</div>
+              <div className="current-price font-size-18">$29.99</div>
+            </div>
+          </div>
+          <div className="hot-ollectibles-item">
+            <div className="wrapper-remaining">
+              <div className="remaining-header font-size-14">Remaining </div>
+              <div className="quantity-remaining font-size-18">26008</div>
+            </div>
           </div>
         </div>
-        <div className="hot-ollectibles-item">
-          <div className="name-label">{item.name}</div>
-        </div>
-        {/*<div className="hot-ollectibles-item">*/}
-        {/*  <div className="wrapper-price">*/}
-        {/*    <div className="price-header font-size-14">Price</div>*/}
-        {/*    <div className="current-price font-size-18">$29.99</div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-        {/*<div className="hot-ollectibles-item">*/}
-        {/*  <div className="wrapper-remaining">*/}
-        {/*    <div className="remaining-header font-size-14">Remaining </div>*/}
-        {/*    <div className="quantity-remaining font-size-18">26008</div>*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </div>
-      {/*</Link>*/}
+      </Link>
     </div>
   );
 };
