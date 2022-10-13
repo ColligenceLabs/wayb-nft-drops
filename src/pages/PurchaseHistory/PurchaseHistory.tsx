170100c867b4899af6bad3b37aacc1ebd5ee7a6b
SonLH <sonlh@chainos.vn>
2022-10-13 18:20:13 +0700

    purchase page new design

--- src/pages/PurchaseHistory/PurchaseHistory.tsx
+++ src/pages/PurchaseHistory/PurchaseHistory.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-multi-carousel/lib/styles.css';
 import { Link } from 'react-router-dom';
+import arrow_blue from '../../assets/icon/arrow_blue.png';
+import arrow_up_right from '../../assets/icon/arrow_up_right.png';
 
 const Purchase_History = () => {
+  const [sortDate, setSortDate] = useState(false);
   return (
     <main className="purchase-container">
       <div className="purchase-history-page">
         <div className="purchase-history-header">
-          <h1>Purchase History</h1>
+          <div>Purchase History</div>
         </div>
         <div className="purchase-history-body">
           <div className="purchase-history-content">
             <div className="table-row heading-row">
-              <div className="title purchase_date">Date</div>
+              <div
+                className="title purchase_date"
+                onClick={() => setSortDate(!sortDate)}
+              >
+                <div className="column-date">
+                  Date
+                  <div className={`arrow-date ${sortDate ? 'asc' : ''}`}>
+                    <img src={arrow_blue} alt="arrow blue" />
+                  </div>
+                </div>
+              </div>
               <div className="title payment_type">Type</div>
               <div className="title name">NFT</div>
               <div className="title amount">Price</div>
@@ -32,8 +45,7 @@ const Purchase_History = () => {
                   Racecarpop 4
                 </a>
                 <Link to="/series" className="name">
-                  It&apos;s a race to the finish! This Pop is raring to go
-                  fourth.
+                  It&apos;s a race to the finish!
                 </Link>
               </div>
               <div className="value amount">$8.99</div>
@@ -41,18 +53,9 @@ const Purchase_History = () => {
               <div className="value explorer_url">0xF3...0B20</div>
               <div className="icon value">
                 <a href="#">
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16 16H2V2H9V0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V9H16V16ZM11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"
-                      fill="black"
-                    ></path>
-                  </svg>
+                  <div className="arrow-up-right">
+                    <img src={arrow_up_right} alt="arrow up right" />
+                  </div>
                 </a>
               </div>
             </div>
@@ -66,7 +69,7 @@ const Purchase_History = () => {
                   Be Rewarded !
                 </a>
                 <Link to="/series" className="name">
-                  Strawberry Shortcake Space Creampop # 49
+                  Strawberry Shortcake
                 </Link>
               </div>
               <div className="value amount">$1.99</div>
@@ -74,18 +77,9 @@ const Purchase_History = () => {
               <div className="value explorer_url">48...48</div>
               <div className="icon value">
                 <a href="#">
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16 16H2V2H9V0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V9H16V16ZM11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"
-                      fill="black"
-                    ></path>
-                  </svg>
+                  <div className="arrow-up-right">
+                    <img src={arrow_up_right} alt="arrow up right" />
+                  </div>
                 </a>
               </div>
             </div>
@@ -99,7 +93,7 @@ const Purchase_History = () => {
                   Ketchupop!
                 </a>
                 <Link to="/series" className="name">
-                  Let&apos;s ketchup some time! It&apos;s been too long...
+                  Let&apos;s ketchup some time!
                 </Link>
               </div>
               <div className="value amount">$19.99</div>
@@ -107,18 +101,9 @@ const Purchase_History = () => {
               <div className="value explorer_url">5V...0G8V</div>
               <div className="icon value">
                 <a href="#">
-                  <svg
-                    width="18"
-                    height="18"
-                    viewBox="0 0 18 18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M16 16H2V2H9V0H2C0.89 0 0 0.9 0 2V16C0 17.1 0.89 18 2 18H16C17.1 18 18 17.1 18 16V9H16V16ZM11 0V2H14.59L4.76 11.83L6.17 13.24L16 3.41V7H18V0H11Z"
-                      fill="black"
-                    ></path>
-                  </svg>
+                  <div className="arrow-up-right">
+                    <img src={arrow_up_right} alt="arrow up right" />
+                  </div>
                 </a>
               </div>
             </div>
