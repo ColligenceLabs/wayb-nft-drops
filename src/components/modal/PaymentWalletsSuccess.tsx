d4384937fcf7d27d64e1a9386247074c169f3ca5
helloljho@gmail.com <helloljho@gmail.com>
2022-10-19 16:30:16 +0900

    bug fix

--- src/components/modal/PaymentWalletsSuccess.tsx
+++ src/components/modal/PaymentWalletsSuccess.tsx
@@ -19,47 +19,56 @@ const PaymentWalletsSuccess: React.FC<PaymentWalletsSuccessProps> = ({
 }) => {
   console.log(itemInfo);
   return (
-    <ReactModal
-      preventScroll={true}
-      isOpen={show}
-      contentLabel="onRequestClose Example"
-      onRequestClose={onHide}
-      className="Modal"
-      overlayClassName="payments-wallets-overlay"
-      shouldCloseOnOverlayClick
-    >
-      <div className="modal-dialog modal-dialog-pay-success">
-        <div className="header">
-          <div className="title">Great!</div>
-          <div className="close-button" onClick={onHide}>
-            <img src={close_icon} alt="icon close" />
+    <>
+      {itemInfo && (
+        <ReactModal
+          preventScroll={true}
+          isOpen={show}
+          contentLabel="onRequestClose Example"
+          onRequestClose={onHide}
+          className="Modal"
+          overlayClassName="payments-wallets-overlay"
+          shouldCloseOnOverlayClick
+        >
+          <div className="modal-dialog modal-dialog-pay-success">
+            <div className="header">
+              <div className="title">Great!</div>
+              <div className="close-button" onClick={onHide}>
+                <img src={close_icon} alt="icon close" />
+              </div>
+            </div>
+            <div className="line"></div>
+            <div className="grid-content">
+              <div className="grid-item sub-title">
+                Your purchase was successfull.
+              </div>
+              <div className="grid-item image-success">
+                <img
+                  src={
+                    isCollection ? itemInfo.itemImage : itemInfo.packageImage
+                  }
+                  alt="image success"
+                />
+              </div>
+              <div className="grid-item title-image">
+                {isCollection ? itemInfo.name : itemInfo.title.en}
+              </div>
+              <div className="grid-item sub-image">
+                Congratulations, your token purchase is in progress. It could
+                take up to ten minutes to verify your transaction anh transfer
+                the token to your wallet.
+              </div>
+              <button
+                className="grid-item button button-got-it"
+                onClick={onHide}
+              >
+                Got it!
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="line"></div>
-        <div className="grid-content">
-          <div className="grid-item sub-title">
-            Your purchase was successfull.
-          </div>
-          <div className="grid-item image-success">
-            <img
-              src={isCollection ? itemInfo.itemImage : itemInfo.packageImage}
-              alt="image success"
-            />
-          </div>
-          <div className="grid-item title-image">
-            {isCollection ? itemInfo.name : itemInfo.title.en}
-          </div>
-          <div className="grid-item sub-image">
-            Congratulations, your token purchase is in progress. It could take
-            up to ten minutes to verify your transaction anh transfer the token
-            to your wallet.
-          </div>
-          <button className="grid-item button button-got-it" onClick={onHide}>
-            Got it!
-          </button>
-        </div>
-      </div>
-    </ReactModal>
+        </ReactModal>
+      )}
+    </>
   );
 };
 
