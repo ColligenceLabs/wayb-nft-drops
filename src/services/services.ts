be1385122c730e8369d3fc57e15a11ff0bfa96d3
helloljho@gmail.com <helloljho@gmail.com>
2022-10-31 14:11:49 +0900

    bug fix

--- src/services/services.ts
+++ src/services/services.ts
@@ -55,7 +55,7 @@ export const getCollectionList = async (isCollection?: boolean) => {
 
 export const getAirdropList = async () => {
   return await customAxios.get(
-    `http://localhost:5001/api/service/mysterybox/airdrops?page=1&limit=5`
+    `/api/service/mysterybox/airdrops?page=1&limit=5`
   );
 };
 
