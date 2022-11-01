933f788beefc7f5537ae41f4c699233ddf6eebbf
Peter H. Nahm <hwnahm@gmail.com>
2022-11-01 13:51:19 +0900

    typo

--- src/components/auth/WalletConnector/KlaytnWallets.tsx
+++ src/components/auth/WalletConnector/KlaytnWallets.tsx
@@ -69,7 +69,7 @@ const KlaytnWallets: React.FC<KlaytnWalletsProps> = ({ close }) => {
         await activate(injected, undefined, true);
         dispatch(setActivatingConnector(injected));
       } else if (id === 1) {
-        console.log(`click ${id}, this is Wallet Connector (Klaytn)`);
+        console.log(`click ${id}, this is Wallet Connect (Klaytn)`);
         // setWalletName('walletConnector');
         const wc = walletconnect(true);
         await activate(wc, undefined, true);
