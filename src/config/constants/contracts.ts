0214845bb61bc2cbbdfd380576bf7c1d99e11c95
Peter H. Nahm <hwnahm@gmail.com>
2022-11-10 17:29:44 +0900

    feat: support quote token for purchase

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -25,4 +25,20 @@ export default {
     [ChainId.BAOBAB]: '0xB4B2E2e00e9d6E5490d55623E4F403EC84c6D33f',
     [ChainId.TMP]: '0x95250dFC15CC25d744c33cC6B458CB3FB6B1Ce3a',
   },
+  usdt: {
+    [ChainId.MAINNET]: '',
+    [ChainId.ROPSTEN]: '',
+    [ChainId.RINKEBY]: '',
+    [ChainId.KLAYTN]: '0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167',
+    [ChainId.BAOBAB]: '0x1459c0eA227092987FC92b824001eBA3d7dB920F',
+    [ChainId.TMP]: '',
+  },
+  usdc: {
+    [ChainId.MAINNET]: '',
+    [ChainId.ROPSTEN]: '',
+    [ChainId.RINKEBY]: '',
+    [ChainId.KLAYTN]: '0x754288077d0ff82af7a5317c7cb8c444d421d103',
+    [ChainId.BAOBAB]: '0x273b95A65856B7A7755471F9f3F109E8AE721e30',
+    [ChainId.TMP]: '',
+  },
 };
